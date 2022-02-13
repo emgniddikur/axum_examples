01ea2a5672266fcb095fb6f9e9ec0541732540e1
emgniddikur <yoshiharu2580@gmail.com>
2022-02-13 22:36:03 +0900

    Add index

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,10 +1,29 @@
-use axum::{response::Html, routing::get, Router};
-use std::net::SocketAddr;
+use axum::{
+    extract::Extension,
+    response::{Html, IntoResponse},
+    routing::get,
+    Json, Router,
+};
+use serde::Serialize;
+use std::{
+    collections::HashMap,
+    net::SocketAddr,
+    sync::{Arc, RwLock},
+};
+use tower_http::add_extension::AddExtensionLayer;
+use uuid::Uuid;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(root));
+    let db = Db::default();
+
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/todos", get(index))
+        .layer(AddExtensionLayer::new(db));
+
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
@@ -13,4 +32,21 @@ async fn main() {
 
 async fn root() -> Html<&'static str> {
     Html("Hello, World!")
+}
+
+#[derive(Clone, Serialize)]
+struct Todo {
+    id: Uuid,
+    text: String,
+    completed: bool,
+}
+
+type Db = Arc<RwLock<HashMap<Uuid, Todo>>>;
+
+async fn index(Extension(db): Extension<Db>) -> impl IntoResponse {
+    let todos = db.read().unwrap();
+
+    let todos = todos.values().cloned().collect::<Vec<_>>();
+
+    Json(todos)
 }
