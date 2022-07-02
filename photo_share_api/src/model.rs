50947727ffcc7092a6f5419ad9167af57df7633b
emgniddikur <yoshiharu2580@gmail.com>
2022-07-02 23:50:35 +0900

    add Mutation to model

--- photo_share_api/src/model.rs
+++ photo_share_api/src/model.rs
@@ -1,12 +1,33 @@
-use async_graphql::{EmptyMutation, EmptySubscription, Object, Schema};
+use async_graphql::{EmptySubscription, Object, Schema};
+use once_cell::sync::Lazy;
+use std::sync::Mutex;
+
+struct Photo {
+    name: String,
+    description: String,
+}
+
+static PHOTOS: Lazy<Mutex<Vec<Photo>>> = Lazy::new(|| Mutex::new(vec![]));
 
 pub struct Query;
 
 #[Object]
 impl Query {
     async fn total_photos(&self) -> usize {
-        42
+        PHOTOS.lock().unwrap().len()
     }
 }
 
-pub type ApiSchema = Schema<Query, EmptyMutation, EmptySubscription>;
+pub struct Mutation;
+
+#[Object]
+impl Mutation {
+    async fn post_photo(&self, name: String, description: String) -> bool {
+        let photo = Photo { name, description };
+        PHOTOS.lock().unwrap().push(photo);
+
+        true
+    }
+}
+
+pub type ApiSchema = Schema<Query, Mutation, EmptySubscription>;
