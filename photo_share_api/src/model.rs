8b4940c437dfabec2e25f9e14cb743da1af8e08e
emgniddikur <yoshiharu2580@gmail.com>
2022-07-03 02:26:45 +0900

    add all_photos

--- photo_share_api/src/model.rs
+++ photo_share_api/src/model.rs
@@ -1,12 +1,15 @@
-use async_graphql::{EmptySubscription, Object, Schema};
+use async_graphql::{EmptySubscription, Object, Schema, SimpleObject};
 use once_cell::sync::Lazy;
 use std::sync::Mutex;
 
+#[derive(Clone, SimpleObject)]
 struct Photo {
+    id: usize,
     name: String,
     description: String,
 }
 
+static SEQUENCE_ID: Lazy<Mutex<usize>> = Lazy::new(|| Mutex::new(1));
 static PHOTOS: Lazy<Mutex<Vec<Photo>>> = Lazy::new(|| Mutex::new(vec![]));
 
 pub struct Query;
@@ -16,17 +19,30 @@ impl Query {
     async fn total_photos(&self) -> usize {
         PHOTOS.lock().unwrap().len()
     }
+
+    async fn all_photos(&self) -> Vec<Photo> {
+        PHOTOS.lock().unwrap().clone()
+    }
 }
 
 pub struct Mutation;
 
 #[Object]
 impl Mutation {
-    async fn post_photo(&self, name: String, description: String) -> bool {
-        let photo = Photo { name, description };
-        PHOTOS.lock().unwrap().push(photo);
+    async fn post_photo(&self, name: String, description: String) -> Photo {
+        let mut id = SEQUENCE_ID.lock().unwrap();
 
-        true
+        let photo = Photo {
+            id: *id,
+            name,
+            description,
+        };
+
+        PHOTOS.lock().unwrap().push(photo.clone());
+
+        *id += 1;
+
+        photo
     }
 }
 
