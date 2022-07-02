6e1b94b94b679c16469025d2d444faceb3db983b
emgniddikur <yoshiharu2580@gmail.com>
2022-07-02 23:51:20 +0900

    fix main

--- photo_share_api/src/main.rs
+++ photo_share_api/src/main.rs
@@ -1,15 +1,15 @@
 mod handlers;
 mod model;
 
-use async_graphql::{EmptyMutation, EmptySubscription, Schema};
+use async_graphql::{EmptySubscription, Schema};
 use axum::{extract::Extension, routing::get, Router};
 use handlers::{graphql_handler, graphql_playground};
-use model::Query;
+use model::{Mutation, Query};
 use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
-    let schema = Schema::build(Query, EmptyMutation, EmptySubscription).finish();
+    let schema = Schema::build(Query, Mutation, EmptySubscription).finish();
 
     let app = Router::new()
         .route("/", get(graphql_playground).post(graphql_handler))
