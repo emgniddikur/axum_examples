4a7b095b464bf07e02ab74080b6c6f7cc6c6ef2e
emgniddikur <yoshiharu2580@gmail.com>
2022-07-14 21:37:51 +0900

    add pool

--- realworld-axum-sqlx/src/main.rs
+++ realworld-axum-sqlx/src/main.rs
@@ -1,15 +1,18 @@
 use axum::{response::Html, routing::get, Router};
 use clap::Parser;
+use sqlx::postgres::PgPoolOptions;
 use std::net::SocketAddr;
 
 use realworld_axum_sqlx::config::Config;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     dotenv::dotenv().ok();
 
     let config = Config::parse();
     println!("{}", config.database_url);
+
+    let pool = PgPoolOptions::new().connect(&config.database_url).await?;
 
     let app = Router::new().route("/", get(root));
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
@@ -17,6 +20,8 @@ async fn main() {
         .serve(app.into_make_service())
         .await
         .unwrap();
+
+    Ok(())
 }
 
 async fn root() -> Html<&'static str> {
