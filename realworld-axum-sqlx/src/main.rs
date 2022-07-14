a2c9c20246c12c962d1f7fbd0cbeb7908c80f210
emgniddikur <yoshiharu2580@gmail.com>
2022-07-14 18:08:34 +0900

    add config

--- realworld-axum-sqlx/src/main.rs
+++ realworld-axum-sqlx/src/main.rs
@@ -1,8 +1,16 @@
 use axum::{response::Html, routing::get, Router};
+use clap::Parser;
 use std::net::SocketAddr;
+
+use realworld_axum_sqlx::config::Config;
 
 #[tokio::main]
 async fn main() {
+    dotenv::dotenv().ok();
+
+    let config = Config::parse();
+    println!("{}", config.database_url);
+
     let app = Router::new().route("/", get(root));
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     axum::Server::bind(&addr)
