273f0ff8985f87302a2b689e2cac1611dcfc2ec0
emgniddikur <yoshiharu2580@gmail.com>
2022-07-14 22:46:19 +0900

    add serve

---
+++ realworld-axum-sqlx/src/http/mod.rs
@@ -0,0 +1,18 @@
+use crate::config::Config;
+use anyhow::Context;
+use axum::{response::Html, routing::get, Router};
+use sqlx::PgPool;
+use std::net::SocketAddr;
+
+pub async fn serve(config: Config, pool: PgPool) -> anyhow::Result<()> {
+    let app = Router::new().route("/", get(root));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .context("")
+}
+
+async fn root() -> Html<&'static str> {
+    Html("Hello, World!")
+}
--- realworld-axum-sqlx/src/main.rs
+++ realworld-axum-sqlx/src/main.rs
@@ -1,9 +1,7 @@
-use axum::{response::Html, routing::get, Router};
 use clap::Parser;
 use sqlx::{migrate, postgres::PgPoolOptions};
-use std::net::SocketAddr;
 
-use realworld_axum_sqlx::config::Config;
+use realworld_axum_sqlx::{config::Config, http};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -16,16 +14,7 @@ async fn main() -> anyhow::Result<()> {
 
     migrate!().run(&pool).await?;
 
-    let app = Router::new().route("/", get(root));
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    http::serve(config, pool).await?;
 
     Ok(())
-}
-
-async fn root() -> Html<&'static str> {
-    Html("Hello, World!")
 }
