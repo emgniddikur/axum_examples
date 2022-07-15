675cdf8a5ac72767140fc893d6cc444ed39dfd45
Yoshiharu Kato <42399693+emgniddikur@users.noreply.github.com>
2022-07-15 20:53:43 +0900

    realworld-axum-sqlx (#4)
    * install uuid
    
    * add get_user
    
    * fix list_user
    
    * add delete_user
    
    * fix create_user
    
    * add username
    
    * fix create_user
    
    * add update_user

--- realworld-axum-sqlx/src/http/users.rs
+++ realworld-axum-sqlx/src/http/users.rs
@@ -1,26 +1,85 @@
 use crate::http::ApiContext;
-use axum::{extract::Extension, routing::get, Json, Router};
+use axum::{
+    extract::{Extension, Path},
+    routing::get,
+    Json, Router,
+};
 use serde::{Deserialize, Serialize};
-use sqlx::query_scalar;
+use sqlx::{query, query_as};
+use uuid::Uuid;
 
 pub fn router() -> Router {
-    Router::new().route("/api/users", get(list_user).post(create_user))
+    Router::new()
+        .route("/api/users", get(list_user).post(create_user))
+        .route(
+            "/api/users/:user_id",
+            get(get_user).patch(update_user).delete(delete_user),
+        )
 }
 
 #[derive(Serialize, Deserialize)]
 struct User {
+    user_id: Uuid,
     username: String,
 }
 
-async fn list_user() -> Json<Vec<User>> {
-    Json(vec![User {
-        username: "foo".to_string(),
-    }])
+async fn list_user(ctx: Extension<ApiContext>) -> Json<Vec<User>> {
+    let users = query_as!(User, r#"select * from "user""#)
+        .fetch_all(&ctx.pool)
+        .await
+        // TODO: error handling
+        .unwrap();
+
+    Json(users)
 }
 
-async fn create_user(ctx: Extension<ApiContext>, Json(req): Json<User>) {
-    query_scalar!(r#"insert into "user" default values"#)
+async fn get_user(ctx: Extension<ApiContext>, Path(id): Path<Uuid>) -> Json<User> {
+    let user = query_as!(User, r#"select * from "user" where user_id = $1"#, id)
         .fetch_one(&ctx.pool)
+        .await
+        // TODO: error handling
+        .unwrap();
+
+    Json(user)
+}
+
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
+}
+
+async fn create_user(ctx: Extension<ApiContext>, Json(req): Json<CreateUser>) {
+    query!(r#"insert into "user" (username) values ($1)"#, req.username)
+        .execute(&ctx.pool)
+        .await
+        // TODO: error handling
+        .unwrap();
+}
+
+#[derive(Deserialize)]
+struct UpdateUser {
+    username: String,
+}
+
+async fn update_user(
+    ctx: Extension<ApiContext>,
+    Path(id): Path<Uuid>,
+    Json(req): Json<UpdateUser>,
+) {
+    query!(
+        r#"update "user" set username = $2 where user_id = $1"#,
+        id,
+        req.username
+    )
+    .execute(&ctx.pool)
+    .await
+    // TODO: error handling
+    .unwrap();
+}
+
+async fn delete_user(ctx: Extension<ApiContext>, Path(id): Path<Uuid>) {
+    query!(r#"delete from "user" where user_id = $1"#, id)
+        .execute(&ctx.pool)
         .await
         // TODO: error handling
         .unwrap();
