97714e6130653427d2bfd37cc5cee60c0aa7a6d1
emgniddikur <yoshiharu2580@gmail.com>
2022-07-15 23:52:56 +0900

    add follows table

--- realworld-axum-sqlx/migrations/20220711165144_create_table.sql
+++ realworld-axum-sqlx/migrations/20220711165144_create_table.sql
@@ -10,3 +10,9 @@ create table users (
     user_id uuid primary key default uuid_generate_v4(),
     username text collate "case_insensitive" unique not null
 );
+
+create table follows (
+    following_user_id uuid not null references users(user_id) on delete cascade,
+    followed_user_id uuid not null references users(user_id) on delete cascade,
+    primary key (following_user_id, followed_user_id)
+);
