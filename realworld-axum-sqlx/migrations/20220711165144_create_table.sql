869aa9b0d6044986ab66bc2bcdb19e6de3702137
emgniddikur <yoshiharu2580@gmail.com>
2022-07-15 16:24:34 +0900

    add create_user

--- realworld-axum-sqlx/migrations/20220711165144_create_table.sql
+++ realworld-axum-sqlx/migrations/20220711165144_create_table.sql
@@ -1,1 +1,5 @@
-create table "user" (user_id uuid primary key)
+create extension if not exists "uuid-ossp";
+
+create table "user" (
+    user_id uuid primary key default uuid_generate_v4()
+)
--- realworld-axum-sqlx/src/http/users.rs
+++ realworld-axum-sqlx/src/http/users.rs
@@ -1,8 +1,10 @@
-use axum::{routing::get, Json, Router};
+use crate::http::ApiContext;
+use axum::{extract::Extension, routing::get, Json, Router};
 use serde::{Deserialize, Serialize};
+use sqlx::query_scalar;
 
 pub fn router() -> Router {
-    Router::new().route("/api/users", get(list_user))
+    Router::new().route("/api/users", get(list_user).post(create_user))
 }
 
 #[derive(Serialize, Deserialize)]
@@ -14,4 +16,11 @@ async fn list_user() -> Json<Vec<User>> {
     Json(vec![User {
         username: "foo".to_string(),
     }])
+}
+
+async fn create_user(ctx: Extension<ApiContext>, Json(req): Json<User>) {
+    query_scalar!(r#"insert into "user" default values"#)
+        .fetch_one(&ctx.pool)
+        .await
+        .unwrap();
 }
