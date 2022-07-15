fb1523c0dd58be4b9a1535d3321e5a4ddbb018c7
emgniddikur <yoshiharu2580@gmail.com>
2022-07-15 12:37:42 +0900

    add api_router

--- realworld-axum-sqlx/src/http/mod.rs
+++ realworld-axum-sqlx/src/http/mod.rs
@@ -2,12 +2,12 @@ mod users;
 
 use crate::config::Config;
 use anyhow::Context;
-use axum::{response::Html, routing::get, Router};
+use axum::Router;
 use sqlx::PgPool;
 use std::net::SocketAddr;
 
 pub async fn serve(config: Config, pool: PgPool) -> anyhow::Result<()> {
-    let app = Router::new().route("/", get(root));
+    let app = api_router();
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
@@ -15,6 +15,6 @@ pub async fn serve(config: Config, pool: PgPool) -> anyhow::Result<()> {
         .context("")
 }
 
-async fn root() -> Html<&'static str> {
-    Html("Hello, World!")
+fn api_router() -> Router {
+    users::router()
 }
