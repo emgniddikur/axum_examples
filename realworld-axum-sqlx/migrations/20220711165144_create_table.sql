8b519f4ee46b017d52796c7585c1408cf9df001a
emgniddikur <yoshiharu2580@gmail.com>
2022-07-15 20:29:09 +0900

    add username

--- realworld-axum-sqlx/migrations/20220711165144_create_table.sql
+++ realworld-axum-sqlx/migrations/20220711165144_create_table.sql
@@ -1,5 +1,12 @@
 create extension if not exists "uuid-ossp";
 
+create collation case_insensitive (
+    provider = icu,
+    locale = 'und-u-ks-level2',
+    deterministic = false
+);
+
 create table "user" (
-    user_id uuid primary key default uuid_generate_v4()
+    user_id uuid primary key default uuid_generate_v4(),
+    username text collate "case_insensitive" unique not null
 )
