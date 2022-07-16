1c76c65e005364638aefaeb51b8a59a8e24ae113
emgniddikur <yoshiharu2580@gmail.com>
2022-07-16 21:34:01 +0900

    add articles table

--- realworld-axum-sqlx/migrations/20220711165144_create_table.sql
+++ realworld-axum-sqlx/migrations/20220711165144_create_table.sql
@@ -16,3 +16,15 @@ create table follows (
     followed_user_id uuid not null references users(user_id) on delete cascade,
     primary key (following_user_id, followed_user_id)
 );
+
+create table articles (
+    article_id uuid primary key default uuid_generate_v4(),
+    user_id uuid not null references users(user_id) on delete cascade,
+    slug text unique not null,
+    title text not null,
+    description text not null,
+    body text not null,
+    tag_list text [] not null,
+    created_at timestamptz not null default now(),
+    updated_at timestamptz not null default now()
+);
