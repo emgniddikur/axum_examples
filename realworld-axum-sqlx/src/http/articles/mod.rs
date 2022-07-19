51c21e37f7dfbb18674ad1151794957a49afcd5d
emgniddikur <yoshiharu2580@gmail.com>
2022-07-20 02:18:30 +0900

    add create_article

--- realworld-axum-sqlx/src/http/articles/mod.rs
+++ realworld-axum-sqlx/src/http/articles/mod.rs
@@ -1,9 +1,35 @@
-use axum::{routing::post, Router};
+use crate::http::ApiContext;
+use axum::{extract::Extension, routing::post, Json, Router};
+use serde::Deserialize;
+use sqlx::query;
+use uuid::Uuid;
 
 pub fn router() -> Router {
     Router::new().route("/articles", post(create_article))
 }
 
-async fn create_article() {
-    println!("create_article");
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct CreateArticle {
+    user_id: Uuid,
+    slug: String,
+    title: String,
+    description: String,
+    body: String,
+    tag_list: Vec<String>,
+}
+
+async fn create_article(ctx: Extension<ApiContext>, Json(req): Json<CreateArticle>) {
+    query!(
+        "insert into articles (user_id, slug, title, description, body, tag_list) values ($1, $2, $3, $4, $5, $6)",
+        req.user_id,
+        req.slug,
+        req.title,
+        req.description,
+        req.body,
+        &req.tag_list,
+    )
+    .execute(&ctx.pool)
+    .await
+    .unwrap();
 }
