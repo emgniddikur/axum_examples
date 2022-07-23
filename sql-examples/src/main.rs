03f2928ac943567e4c4e666939cba8362cdd9d7e
emgniddikur <yoshiharu2580@gmail.com>
2022-07-23 16:17:04 +0900

    add update

--- sql-examples/src/main.rs
+++ sql-examples/src/main.rs
@@ -17,11 +17,20 @@ async fn select(pool: &Pool<Postgres>) {
     println!("{:?}", rows);
 }
 
+async fn update(pool: &Pool<Postgres>) {
+    sqlx::query("update expenses set deposits = 0")
+        .execute(pool)
+        .await
+        .unwrap();
+}
+
 #[async_std::main]
 async fn main() {
     dotenv().ok();
     let database_url = std::env::var("DATABASE_URL").unwrap();
     let pool = PgPoolOptions::new().connect(&database_url).await.unwrap();
+
+    // update(&pool).await;
 
     select(&pool).await;
 }
