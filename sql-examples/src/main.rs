b1aecdf531f22b435d42c49341d58c0182118fd7
emgniddikur <yoshiharu2580@gmail.com>
2022-07-23 15:52:29 +0900

    add Expense

--- sql-examples/src/main.rs
+++ sql-examples/src/main.rs
@@ -1,5 +1,13 @@
 use dotenv::dotenv;
 use sqlx::postgres::PgPoolOptions;
+use uuid::Uuid;
+
+#[derive(Debug)]
+struct Expense {
+    id: Uuid,
+    deposits: i32,
+    withdrawals: i32,
+}
 
 #[async_std::main]
 async fn main() {
@@ -7,7 +15,7 @@ async fn main() {
     let database_url = std::env::var("DATABASE_URL").unwrap();
     let pool = PgPoolOptions::new().connect(&database_url).await.unwrap();
 
-    let row = sqlx::query!("select * from expenses")
+    let row = sqlx::query_as!(Expense, "select * from expenses")
         .fetch_all(&pool)
         .await
         .unwrap();
