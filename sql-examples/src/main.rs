d2561d97a67e2031b806ca43b2269a4f24cb4522
emgniddikur <yoshiharu2580@gmail.com>
2022-07-23 15:33:41 +0900

    select * from expenses

--- sql-examples/src/main.rs
+++ sql-examples/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+use sqlx::postgres::PgPoolOptions;
+
+#[async_std::main]
+async fn main() {
+    dotenv().ok();
+    let database_url = std::env::var("DATABASE_URL").unwrap();
+    let pool = PgPoolOptions::new().connect(&database_url).await.unwrap();
+
+    let row = sqlx::query!("select * from expenses")
+        .fetch_all(&pool)
+        .await
+        .unwrap();
+    println!("{:?}", row);
 }
